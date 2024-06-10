cd40cafc76786426aa9ba602c0d1fff042733126
IrzexBolt <irteza.nayani200@gmail.com>
2024-06-11 03:01:00 +0500

    Add Script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const accountId = 15727; // Constant value cannot be changed once declared
+let accountName = "Muhammad Irteza"; // Variable assigned value can be changed // We simple use Let
+var accountCity = "Moscow"; // Variable assigned value can be changed
+
+accountName = "hussain";
+
+console.log (accountId);
+console.table([accountId, accountName, accountCity]);
+
+// {Scope} Block Scope  
